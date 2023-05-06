433a7d5e3fa6815eb38fdbd2d8b8f555fdaf811f
fpellerano <francisco.pellerano1992@gmail.com>
2023-05-06 18:58:14 -0300

    add category and product router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header.tsx';
+import Footer from "./components/Footer/Footer.tsx";
 import HomePage from './pages/HomePage.tsx';
+import CategoryPage from './pages/CategoryPage.tsx';
+import ProductPage from './pages/ProductPage.tsx';
 
 
 const App: React.FC = () => {
@@ -10,8 +13,10 @@ const App: React.FC = () => {
       <Header />
       <Routes>
         <Route path="" element={<HomePage/>} />
-        {/* Other routes go here */}
+        <Route path="/category" element={<CategoryPage />} />
+        <Route path="/product" element={<ProductPage/>} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
