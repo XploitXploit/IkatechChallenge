880b94a2742ca125bc31d2074aa4f70c6e55caf7
fpellerano <francisco.pellerano1992@gmail.com>
2023-05-06 18:59:05 -0300

    add json mocked list of shoes

---
+++ frontend/src/assets/json/zapatos.js
@@ -0,0 +1,142 @@
+export const zapatos =[
+        {
+            "nombre": "Zapato azul",
+            "color": "azul",
+            "precio": 3000,
+            "referencia": "G40",
+            "foto": "1.png"
+        },
+        {
+            "nombre": "Zapato rojo",
+            "color": "rojo",
+            "precio": 3500,
+            "referencia": "G40",
+            "foto": "2.png"
+        },
+        {
+            "nombre": "Zapato verde",
+            "color": "verde",
+            "precio": 4000,
+            "referencia": "G40",
+            "foto": "3.png"
+        },
+        {
+            "nombre": "Zapato gris",
+            "color": "gris",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "4.png"
+        },
+        {
+            "nombre": "Zapato cafe",
+            "color": "cafe",
+            "precio": 5500,
+            "referencia": "G40",
+            "foto": "5.png"
+        },
+        {
+            "nombre": "Zapato miel",
+            "color": "miel",
+            "precio": 6000,
+            "referencia": "G40",
+            "foto": "6.png"
+        },
+        {
+            "nombre": "Zapato aguamarina",
+            "color": "aguamarina",
+            "precio": 6500,
+            "referencia": "G40",
+            "foto": "7.png"
+        },
+        {
+            "nombre": "Zapato piel",
+            "color": "piel",
+            "precio": 7000,
+            "referencia": "G40",
+            "foto": "8.png"
+        },
+        {
+            "nombre": "Zapato perla",
+            "color": "perla",
+            "precio": 8000,
+            "referencia": "G40",
+            "foto": "9.png"
+        },
+        {
+            "nombre": "Zapato marinado",
+            "color": "marinado",
+            "precio": 8500,
+            "referencia": "G40",
+            "foto": "10.png"
+        },
+        {
+            "nombre": "Zapato azul1",
+            "color": "azul",
+            "precio": 9000,
+            "referencia": "G40",
+            "foto": "11.png"
+        },
+        {
+            "nombre": "Zapato azul2",
+            "color": "azul",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "12.png"
+        },
+        {
+            "nombre": "Zapato azul3",
+            "color": "azul",
+            "precio": 10000,
+            "referencia": "G40",
+            "foto": "13.png"
+        },
+        {
+            "nombre": "Zapato azul4",
+            "color": "azul",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "14.png"
+        },
+        {
+            "nombre": "Zapato azul5",
+            "color": "azul",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "15.png"
+        },
+        {
+            "nombre": "Zapato azul6",
+            "color": "azul",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "16.png"
+        },
+        {
+            "nombre": "Zapato azul7",
+            "color": "azul",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "17.png"
+        },
+        {
+            "nombre": "Zapato azul8",
+            "precio": 15000,
+            "color": "azul",
+            "referencia": "G40",
+            "foto": "18.png"
+        },
+        {
+            "nombre": "Zapato azul9",
+            "color": "azul",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "19.png"
+        },
+        {
+            "nombre": "Zapato azul10",
+            "color": "azul",
+            "precio": 5000,
+            "referencia": "G40",
+            "foto": "20.png"
+        }
+    ]
