d4d4bb31e30a79556bbdc4fcadb67da77f35eabc
fpellerano <francisco.pellerano1992@gmail.com>
2023-05-07 14:39:59 -0300

    add data to json

--- frontend/src/assets/json/zapatos.js
+++ frontend/src/assets/json/zapatos.js
@@ -1,162 +1,183 @@
-export const zapatos =[
-        {
-            "nombre": "Zapato azul",
-            "color": "azul",
-            "precio": 3000,
-            "referencia": "G40",
-            "foto": "1.png",
-            "talle": 41
-        },
-        {
-            "nombre": "Zapato rojo",
-            "color": "rojo",
-            "precio": 3500,
-            "referencia": "G40",
-            "foto": "2.png",
-            "talle": 42
-        },
-        {
-            "nombre": "Zapato verde",
-            "color": "verde",
-            "precio": 4000,
-            "referencia": "G40",
-            "foto": "3.png",
-            "talle": 44
-        },
-        {
-            "nombre": "Zapato gris",
-            "color": "gris",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "4.png",
-            "talle": 31
-        },
-        {
-            "nombre": "Zapato cafe",
-            "color": "cafe",
-            "precio": 5500,
-            "referencia": "G40",
-            "foto": "5.png",
-            "talle": 31
-        },
-        {
-            "nombre": "Zapato miel",
-            "color": "miel",
-            "precio": 6000,
-            "referencia": "G40",
-            "foto": "6.png",
-            "talle": 41
-        },
-        {
-            "nombre": "Zapato aguamarina",
-            "color": "aguamarina",
-            "precio": 6500,
-            "referencia": "G40",
-            "foto": "7.png",
-            "talle": 36
-        },
-        {
-            "nombre": "Zapato piel",
-            "color": "piel",
-            "precio": 7000,
-            "referencia": "G40",
-            "foto": "8.png",
-            "talle": 36
-        },
-        {
-            "nombre": "Zapato perla",
-            "color": "perla",
-            "precio": 8000,
-            "referencia": "G40",
-            "foto": "9.png",
-            "talle": 42
-        },
-        {
-            "nombre": "Zapato marinado",
-            "color": "marinado",
-            "precio": 8500,
-            "referencia": "G40",
-            "foto": "10.png",
-            "talle": 42
-        },
-        {
-            "nombre": "Zapato azul1",
-            "color": "azul",
-            "precio": 9000,
-            "referencia": "G40",
-            "foto": "11.png",
-            "talle": 42
-        },
-        {
-            "nombre": "Zapato azul2",
-            "color": "azul",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "12.png",
-            "talle": 41
-        },
-        {
-            "nombre": "Zapato azul3",
-            "color": "azul",
-            "precio": 10000,
-            "referencia": "G40",
-            "foto": "13.png",
-            "talle": 44
-        },
-        {
-            "nombre": "Zapato azul4",
-            "color": "azul",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "14.png",
-            "talle": 44
-        },
-        {
-            "nombre": "Zapato azul5",
-            "color": "azul",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "15.png",
-            "talle": 42
-        },
-        {
-            "nombre": "Zapato azul6",
-            "color": "azul",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "16.png",
-            "talle": 35
-        },
-        {
-            "nombre": "Zapato azul7",
-            "color": "azul",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "17.png",
-            "talle": 35
-        },
-        {
-            "nombre": "Zapato azul8",
-            "precio": 15000,
-            "color": "azul",
-            "referencia": "G40",
-            "foto": "18.png",
-            "talle": 38
-        },
-        {
-            "nombre": "Zapato azul9",
-            "color": "azul",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "19.png",
-            "talle": 38
-        },
-        {
-            "nombre": "Zapato azul10",
-            "color": "azul",
-            "precio": 5000,
-            "referencia": "G40",
-            "foto": "20.png",
-            "talle": 41
-        }
-    ]
+
+export const zapatos = [
+	{
+		nombre: "Zapato azul",
+		color: "azul",
+		precio: 3000,
+		referencia: "G40",
+		foto: "1.png",
+		talle: 41,
+		descripcion: "Lorem ipsum dolor sit amet. Ut dolor labore ad nihil pariatur cum odio inventore ad dolor explicabo sed dolorem internos. Sit vero iste eum iure voluptatem quo dolor incidunt qui placeat quia. Ex voluptatum perferendis ex incidunt consequatur et repellendus odit id quia corporis vel galisum quod. Aut earum sapiente aut ratione amet et accusantium error. Id iusto sequi in aperiam dolorem et harum delectus et dolores quia vel architecto galisum sit facilis voluptatem sit provident dolores. Qui error soluta quo eaque laboriosam aut ipsa expedita. Ea atque accusamus At delectus maxime ea deleniti aliquam est itaque totam hic voluptas quidem aut quia nostrum. "
+	},
+	{
+		nombre: "Zapato rojo",
+		color: "rojo",
+		precio: 3500,
+		referencia: "G41",
+		foto: "2.png",
+        talle: 42,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato verde",
+		color: "verde",
+		precio: 4000,
+		referencia: "G42",
+		foto: "3.png",
+        talle: 44,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato gris",
+		color: "gris",
+		precio: 5000,
+		referencia: "G43",
+		foto: "4.png",
+        talle: 31,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato cafe",
+		color: "cafe",
+		precio: 5500,
+		referencia: "G44",
+		foto: "5.png",
+        talle: 31,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato miel",
+		color: "miel",
+		precio: 6000,
+		referencia: "G45",
+		foto: "6.png",
+        talle: 41,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato aguamarina",
+		color: "aguamarina",
+		precio: 6500,
+		referencia: "G46",
+		foto: "7.png",
+        talle: 36,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato piel",
+		color: "piel",
+		precio: 7000,
+		referencia: "G47",
+		foto: "8.png",
+        talle: 36,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato perla",
+		color: "perla",
+		precio: 8000,
+		referencia: "G48",
+		foto: "9.png",
+        talle: 42,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato marinado",
+		color: "marinado",
+		precio: 8500,
+		referencia: "G49",
+		foto: "10.png",
+        talle: 42,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul1",
+		color: "azul",
+		precio: 9000,
+		referencia: "G50",
+		foto: "11.png",
+        talle: 42,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul2",
+		color: "azul",
+		precio: 5000,
+		referencia: "G51",
+		foto: "12.png",
+        talle: 41,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul3",
+		color: "azul",
+		precio: 10000,
+		referencia: "G52",
+		foto: "13.png",
+        talle: 44,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul4",
+		color: "azul",
+		precio: 5000,
+		referencia: "G53",
+		foto: "14.png",
+        talle: 44,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul5",
+		color: "azul",
+		precio: 5000,
+		referencia: "G54",
+		foto: "15.png",
+        talle: 42,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul6",
+		color: "azul",
+		precio: 5000,
+		referencia: "G55",
+		foto: "16.png",
+        talle: 35,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul7",
+		color: "azul",
+		precio: 5000,
+		referencia: "G56",
+		foto: "17.png",
+        talle: 35,
+        descripcion:"Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul8",
+		precio: 15000,
+		color: "azul",
+		referencia: "G57",
+		foto: "18.png",
+        talle: 38,
+        descripcion: "Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul9",
+		color: "azul",
+		precio: 5000,
+		referencia: "G58",
+		foto: "19.png",
+        talle: 38,
+        descripcion: "Lorem ipsum dolor sit amet. Est beatae obcaecati aut impedit nemo sed vitae tempore et voluptatum voluptatem id fuga itaque. Aut ducimus quasi aut nisi repudiandae et similique iusto. Est quaerat numquam et voluptas repellat et esse consequuntur et galisum officiis nam officiis magni et velit enim. Et quia quos et optio fuga et nostrum molestias id atque culpa aut repellat corporis hic laborum adipisci."
+	},
+	{
+		nombre: "Zapato azul10",
+		color: "azul",
+		precio: 5000,
+		referencia: "G59",
+		foto: "20.png",
+        talle: 41,
+        descripcion: "Lorem ipsum dolor sit amet. Et voluptas officiis et rerum perspiciatis ut Quis impedit non tempore Quis et perspiciatis libero sit modi adipisci et nemo rerum. Et doloremque enim aut quia ipsam hic repellat accusantium qui modi reprehenderit eos corrupti fugit! Aut veritatis debitis ut soluta error ab quam impedit aut veniam excepturi quo animi dolores sed repudiandae architecto. Id velit debitis aut modi blanditiis At consequatur distinctio et temporibus ratione qui voluptas doloribus ut impedit consectetur."
+	},
+];
